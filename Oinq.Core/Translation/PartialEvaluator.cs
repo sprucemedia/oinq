f04ba672d2c14f5ec3a48236b63c3bb2697a8763
Brian Knight <brianknight10@gmail.com>
2012-06-15 12:37:18 -0400

    Factored out SubtreeEvaluator logic from the PartialEvaluator.

--- Oinq.Core/Translation/PartialEvaluator.cs
+++ Oinq.Core/Translation/PartialEvaluator.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq.Expressions;
 
 namespace Oinq.Core
@@ -36,52 +35,6 @@ namespace Oinq.Core
         {
             // any operation on a query can't be done locally
             return expression.NodeType != ExpressionType.Parameter;
-        }
-
-        /// <summary>
-        /// Evaluates and replaces sub-trees when first candidate is reached (top-down)
-        /// </summary>
-        class SubtreeEvaluator : ExpressionVisitor
-        {
-            private HashSet<Expression> _candidates;
-
-            // constructors
-            private SubtreeEvaluator(HashSet<Expression> candidates)
-            {
-                _candidates = candidates;
-            }
-
-            // internal methods
-            internal static Expression Evaluate(HashSet<Expression> candidates, Expression exp)
-            {
-                return new SubtreeEvaluator(candidates).Visit(exp);
-            }
-
-            // protected override methods
-            protected override Expression Visit(Expression exp)
-            {
-                if (exp == null)
-                {
-                    return null;
-                }
-                if (_candidates.Contains(exp))
-                {
-                    return EvaluateSubtree(exp);
-                }
-                return base.Visit(exp);
-            }
-
-            // private methods
-            private Expression EvaluateSubtree(Expression e)
-            {
-                if (e.NodeType == ExpressionType.Constant)
-                {
-                    return e;
-                }
-                LambdaExpression lambda = Expression.Lambda(e);
-                Delegate fn = lambda.Compile();
-                return Expression.Constant(fn.DynamicInvoke(null), e.Type);
-            }
         }
     }
 }
---
+++ Oinq.Core/Translation/SubtreeEvaluator.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Oinq.Core
+{
+    /// <summary>
+    /// Evaluates and replaces sub-trees when first candidate is reached (top-down)
+    /// </summary>
+    internal class SubtreeEvaluator : ExpressionVisitor
+    {
+        private HashSet<Expression> _candidates;
+
+        // constructors
+        private SubtreeEvaluator(HashSet<Expression> candidates)
+        {
+            _candidates = candidates;
+        }
+
+        // internal methods
+        internal static Expression Evaluate(HashSet<Expression> candidates, Expression exp)
+        {
+            return new SubtreeEvaluator(candidates).Visit(exp);
+        }
+
+        // protected override methods
+        protected override Expression Visit(Expression exp)
+        {
+            if (exp == null)
+            {
+                return null;
+            }
+            if (_candidates.Contains(exp))
+            {
+                return EvaluateSubtree(exp);
+            }
+            return base.Visit(exp);
+        }
+
+        // private methods
+        private Expression EvaluateSubtree(Expression e)
+        {
+            if (e.NodeType == ExpressionType.Constant)
+            {
+                return e;
+            }
+            LambdaExpression lambda = Expression.Lambda(e);
+            Delegate fn = lambda.Compile();
+            return Expression.Constant(fn.DynamicInvoke(null), e.Type);
+        }
+    }
+}
