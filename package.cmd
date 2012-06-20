2881149fee0b2a25277fd0b1288ec4ea21bcfee0
Brian Knight <brianknight10@gmail.com>
2012-06-20 18:08:47 -0400

    Amended .nupkg file to get correct framework name.

--- package.cmd
+++ package.cmd
@@ -1,13 +1,13 @@
 %windir%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe Oinq.sln /t:Clean,Rebuild /p:Configuration=Release /fileLogger
 
-if not exist Download\package\lib\net4 mkdir Download\Package\lib\net4\
-if not exist Download\package\lib\net4-client mkdir Download\Package\lib\net4-client\
+if not exist Download\package\lib\net40 mkdir Download\Package\lib\net40\
+if not exist Download\package\lib\net40-client mkdir Download\Package\lib\net40-client\
 
-copy Oinq.Core\bin\Release\Oinq.Core.dll Download\Package\lib\net4\
-copy Oinq.Core\bin\Release\Oinq.Core.dll Download\Package\lib\net4-client\
+copy Oinq.Core\bin\Release\Oinq.Core.dll Download\Package\lib\net40\
+copy Oinq.Core\bin\Release\Oinq.Core.dll Download\Package\lib\net40-client\
 
-copy Oinq.Core\bin\Release\Oinq.Core.xml Download\Package\lib\net4\
-copy Oinq.Core\bin\Release\Oinq.Core.xml Download\Package\lib\net4-client\
+copy Oinq.Core\bin\Release\Oinq.Core.xml Download\Package\lib\net40\
+copy Oinq.Core\bin\Release\Oinq.Core.xml Download\Package\lib\net40-client\
 
 .nuget\nuget.exe update -self
 .nuget\nuget.exe pack oinq.nuspec -BasePath Download\Package -Output Download
