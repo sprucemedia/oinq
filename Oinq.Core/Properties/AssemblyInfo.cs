c3f1c1669ff9490c6b67538eb4f2b81218cab70e
Brian Knight <brianknight10@gmail.com>
2012-06-21 14:29:02 -0400

    Changed root namespace and assembly name and version.

--- Oinq.Core/Properties/AssemblyInfo.cs
+++ Oinq.Core/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Oinq.Core")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Oinq")]
+[assembly: AssemblyDescription("A LINQ IQueryable provider that targets translation of LINQ expressions into Pig-like languages.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Oinq.Core")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyCompany("Spruce Media")]
+[assembly: AssemblyProduct("Oinq")]
+[assembly: AssemblyCopyright("Copyright © Spruce Media 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.9.1.0")]
--- package.cmd
+++ package.cmd
@@ -3,11 +3,11 @@
 if not exist Download\package\lib\net40 mkdir Download\Package\lib\net40\
 if not exist Download\package\lib\net40-client mkdir Download\Package\lib\net40-client\
 
-copy Oinq.Core\bin\Release\Oinq.Core.dll Download\Package\lib\net40\
-copy Oinq.Core\bin\Release\Oinq.Core.dll Download\Package\lib\net40-client\
+copy Oinq.Core\bin\Release\Oinq.dll Download\Package\lib\net40\
+copy Oinq.Core\bin\Release\Oinq.dll Download\Package\lib\net40-client\
 
-copy Oinq.Core\bin\Release\Oinq.Core.xml Download\Package\lib\net40\
-copy Oinq.Core\bin\Release\Oinq.Core.xml Download\Package\lib\net40-client\
+copy Oinq.Core\bin\Release\Oinq.xml Download\Package\lib\net40\
+copy Oinq.Core\bin\Release\Oinq.xml Download\Package\lib\net40-client\
 
 .nuget\nuget.exe update -self
 .nuget\nuget.exe pack oinq.nuspec -BasePath Download\Package -Output Download
