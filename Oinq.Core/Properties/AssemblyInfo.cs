c3a7750302fedb1ab5b571b662180eadd66ef3e7
Brian Knight <brianknight10@gmail.com>
2012-07-26 11:01:58 -0400

    Added GlobalAssemblyInfo.cs class

--- Oinq.Core/Properties/AssemblyInfo.cs
+++ Oinq.Core/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -8,28 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Oinq")]
 [assembly: AssemblyDescription("A LINQ IQueryable provider that targets translation of LINQ expressions into Pig-like languages.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Spruce Media")]
 [assembly: AssemblyProduct("Oinq")]
-[assembly: AssemblyCopyright("Copyright © Spruce Media 2012")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("d21196a7-fb43-4a30-a176-5d1afb103f9e")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.9.7.*")]
--- Oinq.EdgeSpring/Properties/AssemblyInfo.cs
+++ Oinq.EdgeSpring/Properties/AssemblyInfo.cs
@@ -7,28 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Oinq.EdgeSpring")]
 [assembly: AssemblyDescription("An C# EdgeSpring client")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Spruce Media")]
 [assembly: AssemblyProduct("Oinq.EdgeSpring")]
-[assembly: AssemblyCopyright("Copyright © Spruce Media 2012")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("7bc4c9c7-b27d-4d7f-8395-da57786fbc13")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.9.6.*")]
